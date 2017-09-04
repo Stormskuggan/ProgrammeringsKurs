59261345ebcd369b3ce218c61bf8ca708870ccea
Stormskuggan <thunder.lord@hotmail.com>
2017-09-04 12:26:24 +0200

    Finished Task1 and 2

--- Assignment-Module2/Task1/Program.cs
+++ Assignment-Module2/Task1/Program.cs
@@ -10,6 +10,15 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+
+            int number = 1;
+
+            Console.WriteLine("Number\tSquare\tCube");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", number, Math.Pow(number, 2), Math.Pow(number, 3));
+                number++;
+            }
         }
     }
 }
--- Assignment-Module2/Task2/Program.cs
+++ Assignment-Module2/Task2/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            int radius = 0;
+            double volume = 0;
+
+            Console.Write("Please input integer: ");
+            radius = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= radius; i++)
+            {
+                volume = calculateVolume(i);
+                Console.WriteLine("Spheres volume with radius {0} is {1}", i, volume);
+            }
+
+        }
+
+        static double calculateVolume(int currentRadius)
+        {
+            double volume = 4 / 3 * Math.PI * Math.Pow(currentRadius, 3);
+            return volume;
         }
     }
 }
