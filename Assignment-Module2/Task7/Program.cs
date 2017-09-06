4dee609d99f2ce7e9adb38982c2c9d685fd0eb85
Stormskuggan <thunder.lord@hotmail.com>
2017-09-06 14:33:21 +0200

    finished task7 and started task8

---
+++ Assignment-Module2/Task7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowestNumber;
+            int inputNumber;
+            bool keepIterating = true;
+
+            Console.Write("This app calculates the smallest number input. To stop number collecting enter the number 0.\ninput number: ");
+            lowestNumber = int.Parse(Console.ReadLine());
+            if (lowestNumber == 0)
+            {
+                keepIterating = false;
+            }
+               
+
+            while (keepIterating)
+            {
+                Console.Write("input number: ");
+                inputNumber = int.Parse(Console.ReadLine());
+                if (inputNumber < lowestNumber && inputNumber > 0)
+                {
+                    lowestNumber = inputNumber;
+                }
+                if (inputNumber == 0)
+                {
+                    keepIterating = false;
+                }
+
+            }
+
+            Console.WriteLine("The lowest number was {0}",lowestNumber);
+
+        }
+    }
+}
---
+++ Assignment-Module2/Task8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            addTwoVariables();
+        }
+
+        static int addTwoVariables(int variable1, int variable2)
+        {
+            int sum = variable1 + variable2;
+            return sum;
+        }
+        static float addTwoVariables(float variable1, float variable2)
+        {
+            float sum = variable1 + variable2;
+            return sum;
+        }
+
+    }
+}
