d1f5685ea509f50324c736bd97c2ba63f9028d00
Stormskuggan <thunder.lord@hotmail.com>
2017-09-17 12:50:28 +0200

    Started HW3

---
+++ Assignment-Module3/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Sphere
+    {
+        private int radius;
+
+
+        public Sphere()
+        {
+
+        }
+
+        public Sphere (int radius)
+        {
+
+        }
+
+    }
+}
