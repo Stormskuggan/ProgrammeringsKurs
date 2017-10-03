86a3f506193d231e23fc71a787fc5ce42470d9f5
Stormskuggan <thunder.lord@hotmail.com>
2017-10-03 12:59:04 +0200

    Did some stuff on mod4

---
+++ Assignment-Module4/BWork2/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWork2
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Grade { get; set; }
+        public string Course { get; set; }
+
+
+        public Student(string newName, string newCourse, string newGrade)
+        {
+            Name = newName;
+            Grade = newGrade;
+            Course = newCourse;
+        }
+
+        public string Display()
+        {
+            return "Name: " + Name + " Grade: " + Grade;
+        }
+    }
+}
