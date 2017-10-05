ee1b54c46fd9bc5e701b7e274ee593778b70e816
Stormskuggan <thunder.lord@hotmail.com>
2017-10-06 01:16:22 +0200

    Started task2

---
+++ Assignment-Module5/Task2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string myName;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SaveButton.Click += SaveButton_Click;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            myName = InputNameBox.Text;
+
+        }
+    }
+}
