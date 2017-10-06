4a172b9b597cc34056d3cc09eee4c69fd4b8445d
Stormskuggan <thunder.lord@hotmail.com>
2017-10-06 20:38:47 +0200

    Finished Task2

--- Assignment-Module5/Task2/MainWindow.xaml.cs
+++ Assignment-Module5/Task2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Task2
 {
@@ -22,19 +23,83 @@ namespace Task2
     {
 
         private string myName;
-
-
+        
+        
 
         public MainWindow()
         {
+
             InitializeComponent();
+
+            DisplayButton.Click += DisplayButton_Click;
             SaveButton.Click += SaveButton_Click;
+            
+        }
+
+        private void DisplayButton_Click(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            //string path = Directory.GetCurrentDirectory();
+
+
+            try
+            {
+
+                StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + @"\Names.txt");
+                DisplayLabel.Content = File.ReadAllText(Directory.GetCurrentDirectory() + @"\Names.txt");
+                sr.Close();
+
+            }
+            catch (FileNotFoundException)
+            {
+                DisplayLabel.Content = "File not found. Maybe save a name first";
+            }
+            catch (Exception)
+            {
+                DisplayLabel.Content = "Error";
+            }
+
+
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             //throw new NotImplementedException();
-            myName = InputNameBox.Text;
+
+            try
+            {
+                if (InputNameBox.Text == "")
+                {
+                    throw new FormatException();
+                }
+
+                myName = InputNameBox.Text;
+
+                //StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + @"\Names.txt");
+                StreamWriter sw = File.AppendText(Directory.GetCurrentDirectory() + @"\Names.txt");
+                sw.WriteLine(myName);
+                
+                sw.Close();
+
+                DisplayLabel.Content = "Name has been saved successfully";
+
+            }
+            catch (FormatException)
+            {
+                DisplayLabel.Content = "Please type in a name";
+            }
+            catch (EncoderFallbackException f)
+            {
+                DisplayLabel.Content = f;
+            }
+            catch (IOException i)
+            {
+                DisplayLabel.Content = i;
+            }
+            catch (Exception)
+            {
+                DisplayLabel.Content = "Error";
+            }
 
         }
     }
