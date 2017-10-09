1d05fb8c2a92f06e54acda5423344f7214c0a341
Stormskuggan <thunder.lord@hotmail.com>
2017-10-09 16:56:31 +0200

    Task4

---
+++ Assignment-Module5/Task4/Encryption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    static class Encryption
+    {
+
+        public static string Encode (string text)
+        {
+            text = text.Replace("A", "E");
+            
+            return text;
+        }
+        public static string Decode(string text)
+        {
+
+
+            return text;
+        }
+
+
+    }
+}
---
+++ Assignment-Module5/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //StringBuilder str = new StringBuilder(Console.ReadLine());
+            //Console.WriteLine("The wrong written text is \n{0}", str.ToString());
+            //str.Replace("e", "a");
+            //Console.WriteLine("The correct text is \n{0}", str.ToString());
+
+            int key = 9;
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+
+
+            //ch = (char)((((ch + key) - d) % 26) + d);
+
+            //Console.WriteLine((char)((((ch + key) - d) % 26) + d));
+            Console.WriteLine((char) (ch + key));
+        }
+    }
+}
